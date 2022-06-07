236da8ab15aa5113a59117a2a7951f6a52c51e65
Nemolite <g25092011@mail.ru>
2022-06-07 08:03:09 +0300

    insert

--- cont.php
+++ cont.php
@@ -5,6 +5,16 @@
  * Date: 26.05.2022
  * Time: 11:27
  */
-print_r($_POST);
+//print_r($_POST);
 
 include "connect.php";
+
+if(!empty($_POST)){
+    $email = $_POST['email'];
+    $pass = $_POST['pass'];
+    $check = $_POST['check'];
+
+$sth = $pdo->prepare("INSERT INTO `dtab` SET `email` = :email, `pass` = :pass,`pod` = :pod");
+$sth->execute(array('email' => $email, 'pass' => $pass, 'pod' => $check));
+
+}
